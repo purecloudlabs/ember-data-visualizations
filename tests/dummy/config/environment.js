b6c86a170f082264a479d67ec134431c48d31e83
Drew Gaynor <drew.gaynor@inin.com>
2017-03-08 11:24:49 -0500

    More 2.10 updates.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -3,15 +3,18 @@ module.exports = function (environment) {
     let ENV = {
         modulePrefix: 'dummy',
         environment,
-        baseURL: '/',
+        rootURL: '/',
         locationType: 'auto',
         EmberENV: {
             FEATURES: {
                 // Here you can enable experimental features on an ember canary build
                 // e.g. 'with-controller': true
+            },
+            EXTEND_PROTOTYPES: {
+                // Prevent Ember Data from overriding Date.parse.
+                Date: false
             }
         },
-
         APP: {
             // Here you can pass flags/options to your application instance
             // when it is created
