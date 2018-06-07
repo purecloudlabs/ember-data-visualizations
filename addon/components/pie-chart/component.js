23befa8b66fb471956015333c5eee940b36ba3d6
eileenprice <eileen.price@genesys.com>
2018-06-07 08:43:19 -0400

    initial pie chart work

---
+++ addon/components/pie-chart/component.js
@@ -0,0 +1,38 @@
+// import moment from 'moment';
+// import _ from 'lodash/lodash';
+// import d3 from 'd3';
+import dc from 'dc';
+// import crossfilter from 'crossfilter';
+// import $ from 'jquery';
+import BaseChartComponent from '../base-chart-component';
+
+/**
+   @public
+   @module pie-chart
+   @type component
+   @desc dc.js pie chart
+*/
+export default BaseChartComponent.extend({
+    classNames: ['pie-chart'],
+
+    colors: [
+        '#1f77b4', '#ff7f0e', '#2ca02c',
+        '#9467bd', '#8c564b', '#e377c2',
+        '#7f7f7f', '#bcbd22', '#17becf'
+    ],
+
+    buildChart() {
+        let chart = dc.pieChart(`#${this.get('elementId')}`);
+
+        chart
+            .width(768)
+            .height(480)
+            .slicesCap(20)
+            .innerRadius(100)
+            .dimension(this.get('dimension'))
+            .group(this.get('group')[0])
+            .legent(dc.legend());
+
+        chart.render();
+    }
+});
