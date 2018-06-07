1cb67dba29786c5c46c969400cec64169a1b853e
eileenprice <eileen.price@genesys.com>
2018-06-07 16:26:05 -0400

    working on chart not available view

--- addon/components/line-chart/component.js
+++ addon/components/line-chart/component.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 import _ from 'lodash/lodash';
 import d3 from 'd3';
 import dc from 'dc';
-// import crossfilter from 'crossfilter';
+import crossfilter from 'crossfilter';
 // import $ from 'jquery';
 import BaseChartComponent from '../base-chart-component';
 
@@ -105,6 +105,12 @@ export default BaseChartComponent.extend({
     },
 
     onRenderlet(tip) {
+
+        // This is outside the Ember run loop so check if component is destroyed
+        if (this.get('isDestroyed') || this.get('isDestroying')) {
+            return;
+        }
+
         this.addClickHandlersAndTooltips(d3.select('.line-chart > svg > defs'), tip, 'circle.dot');
         if (this.get('showCurrentIndicator') && this.get('currentInterval')) {
             this.changeTickForCurrentInterval();
@@ -142,5 +148,117 @@ export default BaseChartComponent.extend({
                 currentTick.select('text').html(tickHtml);
             }
         }
+    },
+
+    showChartNotAvailable() {
+        const chartNotAvailableMessage = this.get('chartNotAvailableMessage');
+        const chartNotAvailableColor = this.get('chartNotAvailableColor');
+        const chartNotAvailableTextColor = this.get('chartNotAvailableTextColor');
+        const xAxis = this.get('xAxis');
+        const yAxis = this.get('yAxis');
+
+        let lineChart = dc.lineChart(`#${this.get('elementId')}`);
+        this.set('chart', lineChart);
+
+        const duration = moment.duration(xAxis.domain[1].diff(xAxis.domain[0]));
+        let ticks = 30;
+        if (duration.asMonths() >= 1) {
+            ticks = duration.asDays();
+        } else if (duration.asWeeks() >= 1) {
+            ticks = 30;
+        } else if (duration.asDays() >= 1) {
+            ticks = 24;
+        }
+
+        const data = d3.time.scale().domain(xAxis.domain).ticks(ticks);
+        const filter = crossfilter(data);
+        const dimension = filter.dimension(d => d);
+        const group = dimension.group().reduceCount(g => g);
+
+        lineChart
+            .colors(chartNotAvailableColor)
+            .renderTitle(false)
+            .brushOn(false)
+            .height(this.get('height'))
+            .margins({
+                top: 10,
+                right: 100,
+                bottom: 50,
+                left: 100
+            })
+            .x(d3.time.scale().domain(xAxis.domain))
+            .xUnits(() => data.length + 1)
+            .y(d3.scale.linear().domain([0, 1]))
+            .group(group)
+            .dimension(dimension);
+
+        if (this.get('width')) {
+            this.get('chart').width(this.get('width'));
+        }
+
+        lineChart.on('renderlet', chart => {
+            // This is outside the Ember run loop so check if component is destroyed
+            if (this.get('isDestroyed') || this.get('isDestroying')) {
+                return;
+            }
+
+            // Set up any necessary hatching patterns
+            let svg = d3.select('.line-chart > svg > defs');
+
+            svg
+                .append('clippath')
+                .attr('id', 'topclip')
+                .append('rect')
+                .attr('x', '0')
+                .attr('y', '0')
+                .attr('width', 200)
+                .attr('height', 200);
+            svg
+                .append('pattern')
+                .attr('id', 'chartNotAvailableHatch')
+                .attr('patternUnits', 'userSpaceOnUse')
+                .attr('width', 4)
+                .attr('height', 4)
+                .attr('patternTransform', 'rotate(45)')
+                .append('rect')
+                .attr('x', '0')
+                .attr('y', '0')
+                .attr('width', 2)
+                .attr('height', 4)
+                .attr('fill', chartNotAvailableColor);
+
+            chart.selectAll('rect.bar')
+                .attr('fill', 'url(#chartNotAvailableHatch)')
+                .attr('opacity', '.7')
+                .attr('rx', '2')
+                .attr('stroke', 'white');
+        });
+
+        lineChart.on('postRender', () => {
+
+            // This is outside the Ember run loop so check if component is destroyed
+            if (this.get('isDestroyed') || this.get('isDestroying')) {
+                return;
+            }
+
+            d3.select('.line-chart > svg > text').remove();
+            let svg = d3.select('.line-chart > svg');
+            let bbox = svg.node().getBBox();
+            svg
+                .append('text')
+                .text(chartNotAvailableMessage)
+                .style('fill', chartNotAvailableTextColor)
+                .attr('class', 'chart-not-available')
+                .attr('text-anchor', 'middle')
+                .attr('y', bbox.y + (bbox.height / 2))
+                .attr('x', bbox.x + (bbox.width / 2));
+        });
+        if (xAxis && xAxis.ticks) {
+            this.get('chart').xAxis().ticks(xAxis.ticks);
+        }
+        if (yAxis && yAxis.ticks) {
+            this.get('chart').yAxis().ticks(yAxis.ticks);
+        }
+        lineChart.render();
     }
 });
