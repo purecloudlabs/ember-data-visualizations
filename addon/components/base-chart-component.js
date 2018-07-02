8bb911b8342c52c07575c27c1e87c0e99357a065
eileenprice <eileen.price@genesys.com>
2018-07-02 08:40:06 -0400

    removing dead code

--- addon/components/base-chart-component.js
+++ addon/components/base-chart-component.js
@@ -83,9 +83,8 @@ export default Component.extend({
             return;
         }
 
-        // NOTE: THIS BEING HERE IS ASSUMING ALL CHARTS WILL USE ALL OF THESE PROPERTIES. MAY NOT BE A VALID ASSUMPTION
-        // REQUIRED: group, dimension, xAxis.domain unless !isChartAvailable
-        if (!this.get('group') || /* !this.get('group.0.all') ||*/ !this.get('dimension')) {
+        // REQUIRED: group, dimension unless !isChartAvailable
+        if (!this.get('group') || !this.get('dimension')) {
             return false;
         }
 
