a98631aa61e77319726d7cc72913b5299c72ef27
Shane Bruggeman <shane.bruggeman@genesys.com>
2020-05-04 16:14:12 -0400

    Allow user customizations on column chart story

--- stories/components/column-chart.js
+++ stories/components/column-chart.js
@@ -2,77 +2,127 @@ import hbs from 'htmlbars-inline-precompile';
 import moment from 'moment';
 import crossfilter from 'crossfilter';
 import content from '../content/column-chart-data.json';
+import { computed } from '@ember/object';
+
+/**
+ * Configure the rendered chart
+ *
+ * @param {array} dataSet set of objects
+ * @example [
+ *  { date: "20101101", value: 100 },
+ *  { date: "20101201", value: 150 },
+ *  { date: "20110101", value: 200 }
+ * ]
+ * @param {string} type either 'GROUPED' or 'STACKED'
+ */
+const buildConfigOptions = (dataset, options) => {
+    dataset = JSON.parse(JSON.stringify(dataset));
+
+    // combine dataset values together by date
+    dataset.forEach(function (d) {
+        d.date = moment(d.date, 'YYYYMMDD').toDate();
+    });
+
+    const dimension = crossfilter(dataset).dimension(d => d.date);
+
+    // each datum key gets a bar on the bar chart
+    // GROUPED = bars side by side, STACKED = bars on top of one another
+    const type = options.type || 'GROUPED';
+
+    const groupKeys = dataset.reduce((keys, datum) => {
+        const datumKeys = Object.keys(datum).filter(key => key !== 'date');
+
+        datumKeys.forEach(key => {
+            if (!keys.includes(key)) {
+                keys.push(key);
+            }
+        });
+
+        return keys;
+    }, []);
+
+    const group = groupKeys.map(key => dimension.group().reduceSum(item => item[key] || 0));
+    const series = groupKeys.map(key => ({ title: key, hatch: false }));
+    const colors = ['#3F8BA8', '#2A2C4F', '#558D6C', '#F4880B'];
+
+    const startX = dataset.map(d => d.date).reduce((start, time) => start && moment(start).isBefore(time) ? moment(start) : moment(time), null);
+    const endX = dataset.map(d => d.date).reduce((end, time) => end && moment(end).isAfter(time) ? moment(end) : moment(time), null);
+
+    const xAxis = {
+        domain: [startX, endX],
+        ticks: 3,
+        tickMarks: 3
+    };
+
+    return Object.assign({
+        type,
+        height: 200,
+        group,
+        dimension,
+        series,
+        colors,
+        showLegend: true,
+        xAxis,
+        yAxis: {
+            ticks: 3
+        }
+    }, options);
+};
+
+const initialData = content;
 
 export const columnChartConfigOptions = () => {
     return {
         template: hbs`
-        {{column-chart 
-            dimension=dimension
-            group=group
-            type=type
-            series=series
-            colors=colors
-            height=height
-            xAxis=xAxis
-            yAxis=yAxis
-        }}`,
+            <h1>Column Chart</h1>
+            <button type="button" onclick={{action applyDataset nextDataset}}>Change dataset</button>
+            <button type="button" onclick={{action toggleLegend}}>Toggle Showing Legend</button>
+            <button type="button" onclick={{action reset}}>Reset</button>
+
+            {{column-chart 
+                dimension=config.dimension
+                group=config.group
+                type=config.type
+                series=config.series
+                colors=config.colors
+                height=config.height
+                showLegend=config.showLegend
+                xAxis=config.xAxis
+                yAxis=config.yAxis
+            }}
+        `,
         context: {
-            dimension: dimensions,
-            group: groups,
-            type: 'GROUPED',
-            series: [{ title: 'Offered Calls', hatch: false }],
-            colors,
-            height: 200,
-            xAxis: {
-                domain: [moment('2016-10-31T00:00:00.000Z'), moment('2016-12-03T00:00:00.000Z')],
-                ticks: 3,
-                tickMarks: 3
+            dataset: initialData,
+            nextDataset: initialData,
+            overrides: { showLegend: true },
+
+            config: computed('dataset', 'overrides.{showLegend}', function () {
+                const dataset = this.get('dataset');
+                const overrides = this.get('overrides');
+
+                return buildConfigOptions(dataset, overrides);
+            }),
+
+            reset() {
+                this.set('overrides', {});
             },
-            yAxis: {
-                ticks: 3
+
+            applyDataset(dataset, overrides) {
+                const currentDataset = this.get('dataset');
+
+                if (overrides) {
+                    this.set('overrides', overrides);
+                }
+
+                if (dataset !== currentDataset) {
+                    this.set('nextDataset', currentDataset);
+                }
+            },
+
+            toggleLegend() {
+                const showLegend = this.get('overrides.showLegend');
+                this.set('overrides.showLegend', !showLegend);
             }
         }
     };
 };
-
-let dimensions = {};
-let groups = {};
-let colors = {};
-
-/**
- * @method createDimensions
- * Create the defined dimensions from the controller.
- * @return dimensions
- * @private
- */
-const createDimensions = () => {
-    content.forEach(function (d) {
-        d.date = moment(d.date, 'YYYYMMDD').toDate();
-    });
-
-    dimensions = crossfilter(content).dimension(d => d.date);
-};
-
-/**
- * @method createGroups
- * Create the defined groups from the controller.
- * @return groups
- * @private
- */
-const createGroups = () => {
-    groups = [dimensions.group().reduceSum(item => item.calls)];
-};
-
-/**
- * @method setColors
- * Set the column colors in the chart.
- * @return colors
- * @private
- */
-const setColors = () => {
-    colors = ['#2FCEF5'];
-};
-
-createDimensions();
-createGroups();
-setColors();
